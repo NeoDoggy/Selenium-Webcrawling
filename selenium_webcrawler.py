ff311367013ac8c88ab385959838232da17da6a1
neodoggy <60221051+NeoDoggy@users.noreply.github.com>
2023-11-23 23:41:42 +0800

    Update selenium_webcrawler.py

--- selenium_webcrawler.py
+++ selenium_webcrawler.py
@@ -5,12 +5,9 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 import time
 
-# 設定瀏覽器選項
 options = Options()
-# 建立 driver
 s = Service("./chromedriver-win64/chromedriver-win64/chromedriver.exe")
 chrome = webdriver.Chrome(service=s, options=options)
-# 存取 Website
 url = "https://www.google.com/"
 chrome.get(url)
 
@@ -18,7 +15,5 @@ content = chrome.find_element(By.ID,'APjFqb')
 content.send_keys("hahapiyan")
 content.send_keys(Keys.RETURN)
 
-# 等待 5 秒鐘以載入頁面
 time.sleep(5)
-# 關閉瀏覽器視窗
 chrome.close()
